650648e7038e98503547fe89bd4f7ff125cd61da
JaryZ <43941637+JaryZ@users.noreply.github.com>
2018-11-05 00:00:14 +0200

    Add files via upload

---
+++ KN18 відповіді/дз 1/new 1.js
@@ -0,0 +1,3 @@
+var myArray = new Array ()
+myArray [0] = 'Москва'
+myArray [1] = 'Хабаровськ'
---
+++ KN18 відповіді/дз 2/java 2.js
@@ -0,0 +1,9 @@
+var fruits = ["Понеділок", "Вівторок", "Середа", "Четверг", "Пятниця", "Субота", "Неділя"];
+
+alert( fruits[0] );
+alert( fruits[1] );
+alert( fruits[2] );
+alert( fruits[3] );
+alert( fruits[4] );
+alert( fruits[5] );
+alert( fruits[6] );
